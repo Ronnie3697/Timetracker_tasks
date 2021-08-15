178b2238cbf3bce309a401a26e76ba85d03ff34d
Ronnie3697 <david.nemecek92@gmail.com>
2021-08-15 23:38:22 +0200

    select_all

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+// The Select All Checkbox
+const selectAllCheckbox = document.querySelector(".checkbox-select-all");
+const allSelectCheckboxes = document.querySelectorAll(".select");
+
+selectAllCheckbox.addEventListener("click", () => {
+  if (selectAllCheckbox.checked == true) {
+    for (let i = 0; i < allSelectCheckboxes.length; i++) {
+      allSelectCheckboxes[i].checked = true;
+    }
+  } else {
+    for (let i = 0; i < allSelectCheckboxes.length; i++) {
+      allSelectCheckboxes[i].checked = false;
+    }
+  }
+});
